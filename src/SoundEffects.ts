c7bac24acbdcc3a23818d2813c20a41c07432c99
boyatlanta <34040729+boyatlanta@users.noreply.github.com>
2024-01-10 15:50:01 -0500

    Update SoundEffects.ts

--- src/SoundEffects.ts
+++ src/SoundEffects.ts
@@ -2,31 +2,31 @@ export const soundEffectListMap = [
   {
     name: "Rain",
     imageUrl:
-      "images/.png",
-    src: "https://res.cloudinary.com/dnmjmjdsj/video/upload/v1702247988/audios/rain_oj61ra.mp3",
+      "images/rain.png",
+    src: "audios/rain.mp3",
   },
   {
     name: "Crickets",
     imageUrl:
-      "https://res.cloudinary.com/dnmjmjdsj/image/upload/v1702509247/audios/icons/8_cslz5j.png",
-    src: "https://res.cloudinary.com/dnmjmjdsj/video/upload/v1702248061/audios/crickets_kk9pv5.mp3",
+      "images/crickets.png",
+    src: "audios/crickets.mp3",
   },
   {
     name: "Fire",
     imageUrl:
-      "https://res.cloudinary.com/dnmjmjdsj/image/upload/v1702509295/audios/icons/4_kolblb.png",
-    src: "https://res.cloudinary.com/dnmjmjdsj/video/upload/v1702660090/audios/sounds/campfireinthegarden_ydrgyj.mp3",
+      "images/fire.png",
+    src: "audios/fire..mp3",
   },
   {
     name: "Bell",
     imageUrl:
-      "https://res.cloudinary.com/dnmjmjdsj/image/upload/v1702539357/audios/icons/7_rgwsr8.png",
-    src: "https://res.cloudinary.com/dnmjmjdsj/video/upload/v1702660121/audios/sounds/best-hypnotic-wind-chimes-long-nice-loop_eplorz.mp3",
+      "images/wind.png",
+    src: "audios/wind..mp3",
   },
   {
     name: "Thunder",
     imageUrl:
-      "https://res.cloudinary.com/dnmjmjdsj/image/upload/v1702539337/audios/icons/3_a1etkv.png",
-    src: "https://res.cloudinary.com/dnmjmjdsj/video/upload/v1702540826/audios/icons/thunder-25689_r1bdzg.mp3",
+      "images/thunder.png",
+    src: "audios/thunder..mp3",
   },
 ];
