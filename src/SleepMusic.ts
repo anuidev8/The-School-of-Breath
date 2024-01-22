59c3937a3b226a3b0954a0dece74242e788acc5e
boyatlanta <34040729+boyatlanta@users.noreply.github.com>
2024-01-22 18:03:46 -0500

    Update SleepMusic.ts

--- src/SleepMusic.ts
+++ src/SleepMusic.ts
@@ -4,70 +4,70 @@ export const menuList = [
     name:'ANXIETY RELIEF SLEEP MUSIC 396 Hz',
     image:'images/MWAA0015_and0v7.png',
     audio:'audios/MWAA0015_and0v7.mp3',
-    description:'ANXIETY RELIEF - SLEEP MUSIC | 396 Hz | Dark Screen | Sleep Aid | 8 hour'
+    description:'ANXIETY RELIEF - SLEEP MUSIC | 396 Hz | Sleep Aid'
   },
   {
     id:2,
     name:'CHAKRA BALANCE',
     image:'images/MWAA0016_1_z6qzdz.png',
     audio:'audios/MWAA0016_1_z6qzdz.mp3',
-    description:'Black Screen Sleep Music ☯ Balance Chakras while Sleeping ☯ SOLFEGGIO FREQUENCIES'
+    description:'Balance Chakras while Sleeping ☯ SOLFEGGIO FREQUENCIES'
   },
   {
     id:3,
     name:'REDUCE ANXIETY',
     image:'images/MWAA0032_1_vdhyfa.png',
     audio:'audios/MWAA0032_1_vdhyfa.mp3',
-    description:'BLACK SCREEN SLEEP MUSIC ☯ REDUCE ANXIETY ☯ 528 Hz Music'
+    description:'REDUCE ANXIETY ☯ 528 Hz Music'
   },
   {
     id:4,
     name:'Body Healing solfeggio frequencies',
     image:'images/MWAA0037_1_zzpa2p.png',
     audio:'audios/MWAA0037_1_zzpa2p.mp3',
-    description:'BLACK SCREEN SLEEP MUSIC ☯ All 9 solfeggio frequencies ☯ Body Healing'
+    description:'All 9 solfeggio frequencies ☯ Body Healing'
   },
   {
     id:5,
     name:'Sleep Soundly with Full Body Healing',
     image:'images/MWAA0043_1_zo2idb.png',
     audio:'audios/MWAA0043_1_zo2idb.mp3',
-    description:'Sleep Soundly with Full Body Healing | Black Screen Sleep Music with 528 Hz'
+    description:'Sleep Soundly with Full Body Healing | Sleep Music with 528 Hz'
   },
   {
     id:6,
     name:'Deep Sleep | Full Body Healing',
     image:'images/MWAA0058_vfe3as.png',
     audio:'audios/MWAA0058_vfe3as.mp3',
-    description:'Deep Sleep | Full Body Healing | Solfeggio Frequencies | Black Screen'
+    description:'Deep Sleep | Full Body Healing | Solfeggio Frequencies'
   },
   {
     id:7,
     name:'Fall Asleep Fast 432 Hz🌙✨',
     image:'images/MWAA0066_mhcerv.png',
     audio:'audios/MWAA0066_mhcerv.mp3',
-    description:'Fall Asleep Fast 432 Hz🌙✨ The Power of Theta Waves and Black Screen Sleep Music 🎵'
+    description:'Fall Asleep Fast 432 Hz🌙✨ The Power of Theta Waves with Sleep Music 🎵'
   },
   {
     id:8,
     name:'Sleep Music with Solfeggio Frequencies',
     image:'images/MWAA0070_hxj9s3.png',
     audio:'audios/MWAA0070_hxj9s3.mp3',
-    description:'Black Screen 💤 Full Body Healing Sleep Music with Solfeggio Frequencies'
+    description:'💤 Full Body Healing Sleep Music with Solfeggio Frequencies'
   },
   {
     id:9,
     name:'Full Body Healing with All 9 Solfeggio Frequencies',
     image:'images/MWAA0081_rt5pjf.png',
     audio:'audios/MWAA0081_rt5pjf.mp3',
-    description:'Full Body Healing with All 9 Solfeggio Frequencies ☯ BLACK SCREEN SLEEP MUSIC'
+    description:'Full Body Healing with All 9 Solfeggio Frequencies ☯ '
   },
   {
     id:10,
     name:'Deep Healing Sleep Music',
     image:'images/MWAA0085.png',
     audio:'audios/MWAA0085.mp3',
-    description:'Deep Healing Sleep Music ☯ BLACK SCREEN SLEEP MUSIC'
+    description:'Deep Healing Sleep Music ☯ '
   },
   {
     id:11,
